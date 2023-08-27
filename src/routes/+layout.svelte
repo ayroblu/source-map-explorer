39f2a1477b338f3fb8e1dc4e845a71318a6f9d3e
Ben Lu <ayroblu@gmail.com>
2023-08-27 23:38:25 +0100

    pwa setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	/* eslint-disable svelte/no-at-html-tags */
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+</script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
+
+<slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				icons: [
+					{
+						src: 'favicons/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
